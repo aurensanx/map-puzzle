23402cab8e12e167c14d443c78d1d461ffd83af2
Alberto Aurensanz <aaurensanz@hiberus.com>
2019-11-09 21:27:50 +0100

    Versión definitiva, carga de mapas como jsons

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import {DrawService} from '../../services/draw.service';
 
 @Component({
   selector: 'app-map',
@@ -12,12 +13,20 @@ export class MapComponent implements OnInit {
 
   map: any;
 
-  constructor(private route: ActivatedRoute, private httpClient: HttpClient) {
+  constructor(private route: ActivatedRoute, private drawService: DrawService, private httpClient: HttpClient) {
   }
 
   ngOnInit() {
     this.route.paramMap.subscribe(({params}: any) => {
-      this.map = params.id;
+      // this.map = params.id;
+      // setTimeout(() => {
+      //   this.drawService.draw('#rootSVG');
+      // }, 1000);
+
+      this.httpClient.get(`assets/maps/${params.id}.json`).subscribe((data: any) => {
+        this.drawService.drawJSON('#rootSVG', data);
+      });
+
     });
   }
 
--- src/app/services/draw.service.ts
+++ src/app/services/draw.service.ts
@@ -8,39 +8,47 @@ import {AreaModel} from '../components/map/model';
 })
 export class DrawService {
 
-  private readonly drawData;
-  private readonly onClick: (d: AreaModel) => void;
-
-  getDrawData() {
-    return this.drawData;
+  constructor(private areaService: AreaService) {
   }
 
-  constructor(private areaService: AreaService) {
+  draw = id => {
+    // const svg = d3.select(id);
+    // svg.on("click", function () {
+    //   const mouse = d3.mouse(this);
+    //   console.log(mouse);
+    // });
+    const d3Data: any = d3.select(id).selectAll('path')
+      .attr("idAlberto", 'a');
+    // d3Data._groups[0] = Array.from(d3Data._groups[0]);
+    // console.log(d3Data);
+    d3Data.on('click', d => {
+      console.log(d);
+    });
+  }
 
-    this.onClick = d => {
-      const clickedArea = d3.select(`path#${d.id}`);
-      if (areaService.currentMapArea.id === d.id) {
-        clickedArea.style('fill', 'green');
-        areaService.removeClickedArea(d);
-      } else {
-        clickedArea.style('fill', 'red');
-        clickedArea.transition().delay(1000).transition().style('fill', 'white');
-      }
-      areaService.getRandomArea();
-    };
+  drawJSON = (id, dataPath: AreaModel[]) => {
 
-    this.drawData = {
+    const d3Data = d3.select(id).selectAll('.land')
+      .data(dataPath).enter().append('path')
+      .attr('class', 'land')
+      .attr('d', d => d.d)
+      .attr('id', d => d.id);
 
-      draw: (id, dataPath: AreaModel[]) => {
+    console.log(d3Data);
 
-        d3.select(id).selectAll('.land')
-        // .data(dataPath).enter().append('path')
-          .attr('class', 'land')
-          // .attr('d', d => d.d)
-          // .attr('id', d => d.id)
-          .on('click', this.onClick);
-      }
-    };
+    d3Data.on('click', this.onClick);
+  }
+
+  onClick = (d: AreaModel) => {
+    const clickedArea = d3.select(`path#${d.id}`);
+    if (this.areaService.currentMapArea.id === d.id) {
+      clickedArea.style('fill', 'green');
+      this.areaService.removeClickedArea(d);
+    } else {
+      clickedArea.style('fill', 'red');
+      clickedArea.transition().delay(1000).transition().style('fill', 'white');
+    }
+    this.areaService.getRandomArea();
   }
 
 }
